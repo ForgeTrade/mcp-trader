a7e901016b245b56205504bd57de0db121fc894d
limerc <osmium760@gmail.com>
2025-10-27 20:39:55 +0300

    fix dockerfile

--- providers/binance-rs/build.rs
+++ providers/binance-rs/build.rs
@@ -1,8 +1,21 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Support both local dev (../../pkg/proto) and Docker build (pkg/proto)
+    let provider_proto_path = if std::path::Path::new("../../pkg/proto/provider.proto").exists() {
+        "../../pkg/proto/provider.proto"
+    } else {
+        "pkg/proto/provider.proto"
+    };
+
+    let provider_proto_dir = if std::path::Path::new("../../pkg/proto").exists() {
+        "../../pkg/proto/"
+    } else {
+        "pkg/proto/"
+    };
+
     tonic_build::configure()
         .build_server(true)
         .build_client(false) // Provider only needs server stubs
-        .compile(&["../../pkg/proto/provider.proto"], &["../../pkg/proto/"])?;
+        .compile(&[provider_proto_path], &[provider_proto_dir])?;
 
     // Compile market-report.proto for unified reporting API
     tonic_build::configure()
